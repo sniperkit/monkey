91b454f0a2000d3c101c4037b39b01ca6182959e
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-11 08:52:34 +0200

    refactor package structure

---
+++ cmd/format/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/sniperkit/monkey/pkg/formatter"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	var f []byte
+	var err error
+	if len(args) == 0 {
+		f, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		f, err = ioutil.ReadFile(args[0])
+		if err != nil {
+			fmt.Println("Formatter: cannot read file", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	formatter := formatter.New(string(f))
+	formatter.Format()
+}
